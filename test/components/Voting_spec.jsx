d0c35346dc82421b421a04920d8ce338c1e606a3
sanjoy <sanjoy120@gmail.com>
2015-11-10 20:34:38 +0600

    Initial commit

---
+++ test/components/Voting_spec.jsx
@@ -0,0 +1,38 @@
+import React from 'react/addons';
+import Voting from '../../src/components/Voting';
+import {expect} from 'chai';
+
+const {renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate}
+    = React.addons.TestUtils;
+
+describe('Voting', () => {
+
+    // ...
+
+    it('invokes callback when a button is clicked', () => {
+        let votedWith;
+        const vote = (entry) => votedWith = entry;
+
+        const component = renderIntoDocument(
+            <Voting pair={["Trainspotting", "28 Days Later"]}
+                    vote={vote}/>
+        );
+        const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+        Simulate.click(buttons[0]);
+
+        expect(votedWith).to.equal('Trainspotting');
+    });
+
+    it('renders just the winner when there is one', () => {
+        const component = renderIntoDocument(
+            <Voting winner="Trainspotting" />
+        );
+        const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+        expect(buttons.length).to.equal(0);
+
+        const winner = React.findDOMNode(component.refs.winner);
+        expect(winner).to.be.ok;
+        expect(winner.textContent).to.contain('Trainspotting');
+    });
+
+});
